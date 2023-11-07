4e5d4b85be0a522959355636009bce407ced50bd
Hinano802 <chhi22083@g.nihon-u.ac.jp>
2023-11-07 18:46:55 +0900

    2番終了

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
 export default function App() {
+    const images = [
+        {
+            src: "images/pic1.jpg",
+            alt: "Closeup of a human eye"
+        },
+        {
+            src: "images/pic2.jpg",
+            alt: "Rock that looks like a wave"
+        },
+        {
+            src: "images/pic3.jpg",
+            alt: "Purple and white pansies"
+        },
+        {
+            src: "images/pic4.jpg",
+            alt: "Section of wall from a pharoah's tomb"
+        },
+        {
+            src: "images/pic5.jpg",
+            alt: "Large moth on a leaf"
+        }
+    ]
     return (
       <>
         <h1>Image gallery example</h1>
@@ -11,7 +33,13 @@ export default function App() {
           <div className="overlay"></div>
           <button className="dark">Darken</button>
         </div>
-        <div className="thumb-bar"></div>
+        <div className="thumb-bar">
+            {images.map((pic) => {
+                return(
+                    <img key={pic.src} src={pic.src} alt={pic.alt} />
+                )
+            })}
+        </div>
       </>
     );
   }
